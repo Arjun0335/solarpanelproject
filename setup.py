b34d241aca03884c025f4e1867547ef252639a1e
Arjun Gupta <145846275+Arjun0335@users.noreply.github.com>
2025-03-21 22:55:21 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import find_packages, setup
-setup(
-    name = 'Generative AI Project',
-    version= '0.0.0',
-    author= 'Arjun Gupta',
-    author_email= 'arjungupta0335@gmail.com',
-    packages= find_packages(),
-    install_requires = [ ]
-)
